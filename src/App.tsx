ed969f23365bd348ef51adc81d551d115dd3c1fd
Ipe <iippol@protonmail.com>
2021-10-07 11:28:13 +0300

    themeprovider, globalstyles on styledcomponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import React from "react";
-import "./App.css";
+import { ThemeProvider } from "styled-components";
+import { Container } from "./components/styles/Container.styled";
+import Header from "./components/Header";
+import GlobalStyles from "./components/styles/Global";
+
+const theme = {
+	colors: {
+		header: "#acbbfe",
+		body: "#fff",
+		footer: "#003333",
+	},
+};
 
 function App() {
-	return <div className="App">Hello</div>;
+	return (
+		<ThemeProvider theme={theme}>
+			<>
+				<GlobalStyles />
+				<Header />
+				<Container>
+					<h1>Hello World</h1>
+				</Container>
+			</>
+		</ThemeProvider>
+	);
 }
 
 export default App;
